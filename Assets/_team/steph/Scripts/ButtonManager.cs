bbf0dd3c1e0061b10d86cf688ea214cb13ff93ed
StephaneRaemaekers <StephaneRaemaekers@users.noreply.github.com>
2018-03-21 08:58:01 +0100

    Add skybox , weapon and test scene

---
+++ Assets/_team/steph/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+public void PlayBtn(string PlayScene)
+    {
+        SceneManager.LoadScene(PlayScene);
+
+    }
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+
+}
+
+
